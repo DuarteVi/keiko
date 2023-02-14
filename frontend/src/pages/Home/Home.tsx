b6cb0feef03ed62e5f29544c455b8c513a08d108
Vincent Duarte <vincentdu@theodo.fr>
2023-02-14 16:58:32 +0100

    My first pokemon

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -5,6 +5,13 @@ export const Home = () => {
     <div className={styles.intro}>
       <div>Bienvenue sur ton futur pokédex !</div>
       <div>Tu vas pouvoir apprendre tout ce qu'il faut sur React et attraper des pokemons !</div>
+      <div>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png" alt="Carapuce"></img>
+        <p>Name:</p>
+        <p>Carapuce</p>
+        <p>Number:</p>
+        <p>7</p>
+      </div>
     </div>
   )
 }
